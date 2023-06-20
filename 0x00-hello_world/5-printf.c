7a99e2807080c5cf4fce63fd69f48df427332f4c
lacapella <francesogar23@gmail.com>
2023-06-20 04:33:41 -0700

    4-puts.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h
+/**
+ * main - A program that prints a line using printf function
+ *
+ * Return: Always 0 (success)
+ *\
+int main(void)
+{
+	printf("with proper grammer, but the outcome is apiece of art,\n")
+	return (0);
+}
